5c39bf366e8a6ccfb390e1183939257d10e3f09d
nikita.barkov <Nikita.Barkov@nordigy.ru>
2022-04-17 02:27:04 +0400

    last lesson

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@
 import getArgs from './helpers/args.js';
 import { getAxiosWeather } from './services/api.service.js';
 import { printError, printHelp, printSuccess } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { getKeyValue, saveKeyValue } from './services/storage.service.js';
 
 const saveToken = async (token) => {
     try {
@@ -22,19 +22,39 @@ const saveCity = async (city) => {
     }
 }
 
+const printWeather = async () => {
+    try {
+        const city = await getKeyValue('city');
+        if (!city) {
+            throw new Error("Нужно сохрнать город");
+        }
+        const result = await getAxiosWeather(city);
+        console.log(result);
+    } catch (e) {
+        const code = e?.response?.status;
+        if (code && code !== 200) {
+            printError(`${e.response.status}: ${e.response.statusText}`);
+        } else {
+            printError(e);
+        }
+    }
+
+}
+
 const initCLI = async () => {
     const args = getArgs(process.argv);
     if (args.h) {
         printHelp();
     }
     if (args.s) {
-        console.log(`Saved city ${args.s}`);
+        saveCity(args.s);
     }
     if (args.t) {
         saveToken(args.t);
     }
-    const result = await getAxiosWeather('moscow');
-    console.log(result);
+    if (!Object.keys(args).length) {
+        printWeather();
+    }
 };
 
 
